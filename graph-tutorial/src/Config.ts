9215bd270fae03faab7850ec8ff9ecebe90272f2
VolkerMargenfeld <vwmvwm@gmail.com>
2020-07-01 08:54:37 +0200

    inital copy

---
+++ graph-tutorial/src/Config.ts
@@ -0,0 +1,9 @@
+export const config = {
+  appId: '0b96c089-67a9-4bb7-a8bb-48ece4c436eb',
+  redirectUri: 'http://localhost:3000',
+  authority: "https://login.microsoftonline.com/64322308-09a9-47a3-8c1c-b82871d60568",
+  scopes: [
+    'user.read',
+    'calendars.read'
+  ]
+};
